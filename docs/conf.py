c16ec57ee84e802372107d104f7cf63800f255d4
Pavle Portic <pavle.portic@tilda.center>
2020-01-29 12:57:47 +0100

    Finalize documentation config

--- docs/conf.py
+++ docs/conf.py
@@ -11,10 +11,20 @@
 import os
 import sys
 
+from pallets_sphinx_themes import ProjectLink
 from sphinx.ext import autodoc
 
 sys.path.insert(0, os.path.abspath('.'))
 
+# Project --------------------------------------------------------------
+
+project = "Deploy-Webhook"
+copyright = "2020 Pavle Portic"
+author = "Pavle Portic"
+
+# General --------------------------------------------------------------
+
+master_doc = "index"
 extensions = [
 	'pallets_sphinx_themes',
 	'sphinx.ext.autodoc',
@@ -23,20 +33,28 @@ extensions = [
 	'sphinx_issues',
 	'sphinxcontrib.autohttp.flask',
 ]
-
-primary_domain = 'py'
-default_role = 'py:obj'
-
-intersphinx_mapping = {'python': ('https://python.readthedocs.io/en/latest/', None)}
-
-source_suffix = '.rst'
-master_doc = 'index'
+intersphinx_mapping = {"python": ("https://docs.python.org/3/", None)}
+issues_github_path = 'TheEdgeOfRage/deploy-webhook'
 exclude_patterns = ['_build']
+source_suffix = '.rst'
 
-project = 'deploy-webhook'
-copyright = '2020 Pavle Portic'
+# HTML -----------------------------------------------------------------
 
-html_theme = 'flask'
+html_theme = "werkzeug"
+html_context = {
+	"project_links": [
+		ProjectLink("Source Code", "https://github.com/TheEdgeOfRage/deploy-webhook/"),
+		ProjectLink("Issue Tracker", "https://github.com/TheEdgeOfRage/deploy-webhook/issues/"),
+		ProjectLink("Docker Hub", "https://hub.docker.com/r/theedgeofrage/deploy-webhook"),
+	]
+}
+html_sidebars = {
+	"index": ["project.html", "localtoc.html", "searchbox.html"],
+	"**": ["localtoc.html", "relations.html", "searchbox.html"],
+}
+singlehtml_sidebars = {"index": ["project.html", "localtoc.html"]}
+html_title = "Deploy-Webhook documentation"
+html_show_sourcelink = False
 
 
 class SimpleDocumenter(autodoc.MethodDocumenter):
